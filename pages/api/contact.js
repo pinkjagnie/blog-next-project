7a71f45c7f79cb452afd071aef281b60d48fd8d0
OlgaKrzysztofik <olga.krzysztofik@gmail.com>
2022-09-01 21:52:20 +0200

    Storing messages into MongoDB as database

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,4 +1,8 @@
-function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+import config from "../../lib/config";
+
+async function handler(req, res) {
   if (req.method === "POST") {
     const { email, name, message } = req.body;
 
@@ -14,14 +18,33 @@ function handler(req, res) {
       return;
     }
 
-    // Store it in a database
     const newMessage = {
       email,
       name,
       message,
     };
 
-    console.log(newMessage);
+    let client;
+
+    try {
+      client = await MongoClient.connect(`${config.urlMongo}`);
+    } catch (error) {
+      res.status(500).json({ message: "Could not connected to the database" });
+      return;
+    }
+
+    const db = client.db();
+
+    try {
+      const result = await db.collection("messages").insertOne(newMessage);
+      newMessage.id = result.insertedId;
+    } catch (error) {
+      client.close();
+      res.status(500).json({ message: "Storing massage failed" });
+      return;
+    }
+
+    client.close();
 
     res
       .status(201)
